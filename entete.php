a99326b0f75449000978c935dda9590edad27419
Kossi Ezou <kmezou@madisoncollege.edu>
2022-01-25 15:56:01 -0600

    Header pages

---
+++ entete.php
@@ -0,0 +1,16 @@
+
+
+
+
+
+<div>
+	<nav>
+		
+		<ul>
+			<li><a href ="inscription.php">Sign Up</a></li>
+			<li><a href ="login.php">Sign In</a></li>
+			<li><img  src ="crm5.jpg" width ="100" height ="75" /></li>
+		</ul>
+	</nav>
+	
+</div>
---
+++ entete2.php
@@ -0,0 +1,14 @@
+
+
+
+
+<div>
+	<nav>
+		
+		<ul>
+			<li><a href ="deconnection.php">Sign Out</a></li>
+			<li><img  src ="crm5.jpg" width ="100" height ="75" /></li>
+		</ul>
+	</nav>
+	
+</div>
