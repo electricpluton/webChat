454432c2b9672686eb37dea94e0ce7c05be5484f
Kossi Ezou <kmezou@madisoncollege.edu>
2022-01-25 15:59:24 -0600

    footer page

---
+++ pied_page.php
@@ -0,0 +1,15 @@
+
+
+
+<div >
+	
+	<div id="texte" > Webchat - Copyright </div>
+	
+	<nav>
+		<ul>
+			<li><a href ="about.php">About </a></li>
+			<li><a href ="contact.php">Contact us</a></li>
+		</ul>
+	</nav>
+	
+</div>
